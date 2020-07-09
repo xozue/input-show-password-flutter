32564bc7c4c9159eb1e638dbba74e0f079611ae7
Josue Don <eljoz89@gmail.com>
2020-07-09 17:04:10 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/input_form_show_password/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.input_form_show_password
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Show and hide password',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Show and hide password'),
+        ),
+        body: Form(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: <Widget>[
+              PasswordInputFormField(),
+              PasswordInputFormField(
+                isPassword: true,
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordInputFormField extends StatefulWidget {
+  final bool isPassword;
+
+  const PasswordInputFormField({this.isPassword = false});
+  @override
+  _PasswordInputFormFieldState createState() => _PasswordInputFormFieldState();
+}
+
+class _PasswordInputFormFieldState extends State<PasswordInputFormField> {
+  bool showPass;
+  @override
+  initState() {
+    showPass = !widget.isPassword;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: !showPass,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        suffixIcon: IconButton(
+          icon: Icon(Icons.remove_red_eye),
+          onPressed: () {
+            setState(() {
+              showPass = !showPass;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
